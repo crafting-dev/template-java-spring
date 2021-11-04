ead7a3068d809cd95547cfdcc1648f2d3ea76df8
i <i@alexandermaru.com>
2021-11-04 15:44:42 +0000

    Add a /ping route for templates to ping pong

---
+++ ping/src/main/java/com/demo/ping/PingApplication.java
@@ -0,0 +1,28 @@
+package com.demo.ping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.json.JSONObject;
+import java.time.Instant;
+
+@SpringBootApplication
+@RestController
+public class PingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String ping(@RequestParam(defaultValue = "To ping, or not to ping; that is the question.") String ping) {
+		JSONObject pong = new JSONObject();
+
+		pong.put("ping", ping);
+		pong.put("received_at", Instant.now());
+
+		return pong.toString();
+	}
+}
